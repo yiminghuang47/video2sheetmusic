74220088f60925b1c54f42d931a8f3c6ea796b6d
yiminghuang47 <yiminghuang47@gmail.com>
2024-08-06 18:12:47 -0400

    test

--- server/server.js
+++ server/server.js
@@ -135,18 +135,18 @@ app.post("/upload", async (req, res) => {
 
 app.post("/youtube-upload", async (req, res) => {
 
-    try {
-        //console.log("test");
-        // const url = req.body.url;
-        // const regions = JSON.parse(req.body.regions);
-        // const { x, y, width, height } = regions[0];
-        // const Y0 = y;
-        // const Y1 = y + height;
-        // const X0 = x;
-        // const X1 = x + width;
-        // console.log(X0 + " " + Y0 + " " + X1 + " " + Y1);
+    //try {
+        console.log("test");
+        const url = req.body.url;
+        const regions = JSON.parse(req.body.regions);
+        const { x, y, width, height } = regions[0];
+        const Y0 = y;
+        const Y1 = y + height;
+        const X0 = x;
+        const X1 = x + width;
+        console.log(X0 + " " + Y0 + " " + X1 + " " + Y1);
 
-        // console.log(url);
+        console.log(url);
 
         // const pythonProcess = spawn("python", [
         //     "script_youtube.py",
@@ -182,10 +182,10 @@ app.post("/youtube-upload", async (req, res) => {
         //     res.setHeader("Content-Type", "application/pdf");
         //     res.send(output);
         // });
-    } catch (error) {
-        console.error("Error in /youtube-upload:", error);
-        res.status(500).send({ error: "Internal Server Error" + error });
-    }
+   // } catch (error) {
+     //   console.error("Error in /youtube-upload:", error);
+       // res.status(500).send({ error: "Internal Server Error" + error });
+    //}
 });
 
 // start the Express server
