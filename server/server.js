1c8d2de62d4f5f4bc49322801a300da3f0fd6f09
yiminghuang47 <yiminghuang47@gmail.com>
2024-08-06 18:03:04 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -134,57 +134,57 @@ app.post("/upload", async (req, res) => {
 });
 
 app.post("/youtube-upload", async (req, res) => {
-    res.send("testing")
-    // try {
-    //     const url = req.body.url;
-    //     const regions = JSON.parse(req.body.regions);
-    //     const { x, y, width, height } = regions[0];
-    //     const Y0 = y;
-    //     const Y1 = y + height;
-    //     const X0 = x;
-    //     const X1 = x + width;
-    //     console.log(X0 + " " + Y0 + " " + X1 + " " + Y1);
 
-    //     console.log(url);
+    try {
+        const url = req.body.url;
+        const regions = JSON.parse(req.body.regions);
+        const { x, y, width, height } = regions[0];
+        const Y0 = y;
+        const Y1 = y + height;
+        const X0 = x;
+        const X1 = x + width;
+        console.log(X0 + " " + Y0 + " " + X1 + " " + Y1);
 
-    //     const pythonProcess = spawn("python", [
-    //         "script_youtube.py",
-    //         url,
-    //         X0,
-    //         Y0,
-    //         X1,
-    //         Y1,
-    //     ]);
+        console.log(url);
 
-    //     let stdoutData = [];
+        // const pythonProcess = spawn("python", [
+        //     "script_youtube.py",
+        //     url,
+        //     X0,
+        //     Y0,
+        //     X1,
+        //     Y1,
+        // ]);
 
-    //     pythonProcess.stdout.on("data", (data) => {
-    //         stdoutData.push(data);
-    //     });
+        // let stdoutData = [];
 
-    //     pythonProcess.stderr.on("data", (data) => {
-    //         console.error(`stderr: ${data}`);
-    //     });
+        // pythonProcess.stdout.on("data", (data) => {
+        //     stdoutData.push(data);
+        // });
 
-    //     pythonProcess.on("close", async (code) => {
-    //         if (code !== 0) {
-    //             return res
-    //                 .status(500)
-    //                 .send({ error: "Error executing python script" });
-    //         }
+        // pythonProcess.stderr.on("data", (data) => {
+        //     console.error(`stderr: ${data}`);
+        // });
 
-    //         const output = Buffer.concat(stdoutData);
-    //         if (output.length === 0) {
-    //             return res.status(500).send({ error: "No PDF generated" });
-    //         }
+        // pythonProcess.on("close", async (code) => {
+        //     if (code !== 0) {
+        //         return res
+        //             .status(500)
+        //             .send({ error: "Error executing python script" });
+        //     }
 
-    //         res.setHeader("Content-Type", "application/pdf");
-    //         res.send(output);
-    //     });
-    // } catch (error) {
-    //     console.error("Error in /youtube-upload:", error);
-    //     res.status(500).send({ error: "Internal Server Error" + error });
-    // }
+        //     const output = Buffer.concat(stdoutData);
+        //     if (output.length === 0) {
+        //         return res.status(500).send({ error: "No PDF generated" });
+        //     }
+
+        //     res.setHeader("Content-Type", "application/pdf");
+        //     res.send(output);
+        // });
+    } catch (error) {
+        console.error("Error in /youtube-upload:", error);
+        res.status(500).send({ error: "Internal Server Error" + error });
+    }
 });
 
 // start the Express server
