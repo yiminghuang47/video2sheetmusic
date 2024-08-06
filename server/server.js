a70603d992d49d3feeb7ab51f170afef1fbb66ec
yiminghuang47 <yiminghuang47@gmail.com>
2024-08-06 18:27:48 -0400

    test

--- server/server.js
+++ server/server.js
@@ -148,16 +148,16 @@ app.post("/youtube-upload", async (req, res) => {
         // console.log(X0 + " " + Y0 + " " + X1 + " " + Y1);
 
         // console.log(url);
+       
+        const pythonProcess = spawn("python", [
+            "script_youtube.py",
+            url,
+            X0,
+            Y0,
+            X1,
+            Y1,
+        ]);
         res.send("test")
-        // const pythonProcess = spawn("python", [
-        //     "script_youtube.py",
-        //     url,
-        //     X0,
-        //     Y0,
-        //     X1,
-        //     Y1,
-        // ]);
-
         // let stdoutData = [];
 
         // pythonProcess.stdout.on("data", (data) => {
