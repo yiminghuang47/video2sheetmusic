9bbd70097864ee61f084db1b461461ad31e0cbc2
yiminghuang47 <yiminghuang47@gmail.com>
2024-08-07 07:45:48 -0400

    updated API_URL

--- client/src/components/BothUpload.jsx
+++ client/src/components/BothUpload.jsx
@@ -13,8 +13,9 @@ import dotenv from 'dotenv'
 
 export default function BothUpload() {
     //const API_URL = import.meta.env.VITE_API_URL;
-    const API_URL = "https://extract-sheet-music-from-video-server.vercel.app";
+    //const API_URL = "https://extract-sheet-music-from-video-server.vercel.app";
    // const API_URL = "http://localhost:5050";
+   const API_URL = "https://vercel.live/link/extract-sheet-music-from-vid-git-1e65f6-yiminghuang47s-projects.vercel.app?via=deployment-domains-list-branch";
     const inputRef = useRef();
     const [urlInput, setUrlInput] = useState("");
     const [url, setUrl] = useState("");
